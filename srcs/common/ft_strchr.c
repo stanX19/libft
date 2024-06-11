e11e793bc895788d037e623471aae2f5652ec823
StanX19 <tanshanchien@gmail.com>
2024-06-11 22:19:27 +0800

    map (argv) check complete

--- srcs/common/ft_strchr.c
+++ srcs/common/ft_strchr.c
@@ -3,15 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shatan <shatan@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*   By: stan <shatan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 21:36:48 by stan              #+#    #+#             */
-/*   Updated: 2024/04/18 15:52:11 by shatan           ###   ########.fr       */
+/*   Updated: 2024/06/11 20:33:28 by stan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// not found: NULL
+// found: char* ptr
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
