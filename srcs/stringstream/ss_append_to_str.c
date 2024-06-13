0cfdbd58007f4fd99d28ad9e009a9846d39bef13
StanX19 <tanshanchien@gmail.com>
2024-06-13 22:19:23 +0800

    norme

--- srcs/stringstream/ss_append_to_str.c
+++ srcs/stringstream/ss_append_to_str.c
@@ -6,7 +6,7 @@
 /*   By: stan <shatan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 12:36:41 by shatan            #+#    #+#             */
-/*   Updated: 2024/06/13 19:04:01 by stan             ###   ########.fr       */
+/*   Updated: 2024/06/13 22:18:55 by stan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ t_stringstream	*ss_append(t_stringstream *ss, const char *str)
 	if (ss->str == NULL)
 	{
 		ss->str = original;
-		return NULL;
+		return (NULL);
 	}
 	ss->pos = ss->str + idx;
 	free(original);
