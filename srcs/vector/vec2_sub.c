d503b138bb8fbdf5da8916ba7fe8c79c5ea00d9c
Tan Shan Chien <tanshanchien@gmail.com>
2024-06-17 18:49:20 +0800

    bfs fine tuning, safe hypot

--- srcs/vector/vec2_sub.c
+++ srcs/vector/vec2_sub.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   vector.h                                           :+:      :+:    :+:   */
+/*   vec2_sub.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shatan <shatan@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/12 15:33:22 by shatan            #+#    #+#             */
-/*   Updated: 2024/06/17 17:41:55 by shatan           ###   ########.fr       */
+/*   Created: 2024/02/02 14:36:37 by shatan            #+#    #+#             */
+/*   Updated: 2024/06/17 17:42:26 by shatan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef VECTOR_H
-# define VECTOR_H
+#include "libft.h"
 
-typedef struct s_vec2
+t_vec2	vec2_sub(t_vec2 v1, t_vec2 v2)
 {
-	int					x;
-	int					y;
-}						t_vec2;
-
-t_vec2					vec2_add(t_vec2 v1, t_vec2 v2);
-t_vec2					vec2_sub(t_vec2 v1, t_vec2 v2);
-double					vec2_hypot(t_vec2 v);
-
-#endif  //VECTOR
+	return ((t_vec2){v1.x - v2.x, v1.y - v2.y});
+}
